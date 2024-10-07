bc2f6870685299fb6f290d42793adb59195ffe34
Joshua-Fish-Student <jofid7d@student.oaisd.org>
2024-10-07 17:09:02 +0000

    did what the doc said to do

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("My name is Joshua Fish.");
+
+
+        int age= 16;
+        string greeting= "Hello World";
+        string firstname= "Joshua";
+        string lastname = "Fish";
+        double money= 2.69;
+
+        Console.WriteLine(greeting + "!" + " My name is " + firstname + " " + lastname + ". I am " + age + " years old. I just spent $" + money + " on an Energy Drink.");
+    }
+}
